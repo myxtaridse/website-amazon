75532faeeb41dd43444a424b4158683e528bc81c
myxtaridse <rustambyr69@gmail.com>
2024-01-10 23:16:21 +0300

    Save cart in localStorage

--- script/cart.js
+++ script/cart.js
@@ -1,13 +1,21 @@
-export let cart = [{
-    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-    quantity: 4
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}, {
-    productId: '3ebe75dc-64d2-4137-8860-1f5a963e534b',
-    quantity: 2
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = [{
+        productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+        quantity: 4
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }, {
+        productId: '3ebe75dc-64d2-4137-8860-1f5a963e534b',
+        quantity: 2
+    }];
+}
+    
+function saveOrders() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -30,7 +38,10 @@ export function addToCart(productId) {
             quantity
         });
     }
+    saveOrders();
 }
+
+
 
 export function removeProduct(productId) {
 //функция возьмет идентификатор продукта и удалить его из корзины
@@ -42,4 +53,5 @@ export function removeProduct(productId) {
         }
     });
     cart = newCart;
+    saveOrders();
 }
