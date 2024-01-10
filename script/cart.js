1bc0d7021501743b7d0efd977f43edd2611b8956
myxtaridse <rustambyr69@gmail.com>
2024-01-10 18:41:39 +0300

    update checkout

--- script/cart.js
+++ script/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [{
+    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    quantity: 4
+}, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+}, {
+    productId: '3ebe75dc-64d2-4137-8860-1f5a963e534b',
+    quantity: 2
+}];
 
 export function addToCart(productId) {
     let matchingItem;
---
+++ script/checkout.js
@@ -0,0 +1,101 @@
+import {cart} from './cart.js';
+import {products} from './data.js';
+
+let cartSummaryHTML = '';
+
+cart.forEach((cartItem) =>{
+    
+    const productId = cartItem.productId;
+
+    let matchingProduct;
+
+    //тут проверим соответствует ли свойство ID нашему идентификатору продукта
+    products.forEach((product) =>{
+        if (product.id === productId) { //если равны, значит сохраним в переменную наш товар
+            matchingProduct = product;
+        }
+    });
+
+    cartSummaryHTML += `
+    <div class="product-container-order js-order-${matchingProduct.id}">
+        <div class="title-order">
+            Delivery date: Thursday, January 11
+        </div>
+
+        <div class="order-sections">
+            <div class="image-order">
+                <img src="${matchingProduct.image}" alt="" class="image-product">
+            </div>
+            
+            <div class="about-order">
+                <div class="title-product">
+                    ${matchingProduct.name}
+                </div>
+
+                <div class="price-product">$${matchingProduct.priceCents / 100}</div>
+                <div class="quantity-product">
+                    <div class="quantity">
+                        Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                     </div>
+                    <button class="link-quantity">
+                        Update
+                    </button>
+                    <button class="link-quantity">
+                        Delete
+                    </button>
+                </div>
+            </div>
+
+            <div class="deadlines-data-order">
+                <div class="deadline-title">
+                    Choose a delivery option:
+                </div>
+
+                <div class="delivery-container">
+                    <input class="delivery-input" type="radio">
+
+                    <div class="delivery-data-shipping">
+                        <div class="delivery-data">
+                            Friday, January 19
+                        </div>
+                        <div class="delivery-shipping">
+                            FREE Shipping
+                        </div>
+                    </div>
+                </div>
+
+                <div class="delivery-container">
+                    <input class="delivery-input" type="radio">
+                
+                    <div class="delivery-data-shipping">
+                        <div class="delivery-data">
+                            Monday, January 15
+                        </div>
+                        <div class="delivery-shipping">
+                            $4.99 - Shipping
+                        </div>
+                    </div>
+                </div>
+
+                <div class="delivery-container">
+                    <input class="delivery-input" type="radio">
+                    
+                    <div class="delivery-data-shipping">
+                        <div class="delivery-data">
+                            Thursday, January 11
+                        </div>
+                        <div class="delivery-shipping">
+                            $9.99 - Shipping
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+
+        </div>
+
+    </div>
+    `;
+});
+
+document.querySelector('.js-container-products').innerHTML = cartSummaryHTML;
